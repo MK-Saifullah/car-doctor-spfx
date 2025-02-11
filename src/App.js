2337cc92f0ee7c353f96585650d83622898d4a3d
Khalid Saifullah <khalid@lyftaskrot.se>
2025-02-11 11:31:42 +0100

    added RouteChangeListener component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { RouterProvider } from 'react-router-dom';
-import './App.css';
-import router from './Pages/Router/Routes/Routes';
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import router from "./Pages/Router/Routes/Routes";
+import RouteChangeListener from "./RouteChangeListener";
 
 function App() {
-
-
-
   return (
-    <div className='mx-auto max-w-screen-lg'>
-      <RouterProvider router = {router}></RouterProvider>
+    <div className="mx-auto max-w-screen-lg">
+      {/* <RouteChangeListener>
+      </RouteChangeListener> */}
+      
+        <RouterProvider router={router}>
+        <RouteChangeListener />
+        </RouterProvider>
     </div>
   );
 }
